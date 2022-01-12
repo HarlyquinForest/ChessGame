c5478b7dbe2d37b531e22b60cd48c6c55403009d
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-01-12 12:06:06 +0330

    king movement implemented

--- src/amin/main/chess/Pieces/King.java
+++ src/amin/main/chess/Pieces/King.java
@@ -14,6 +14,22 @@ public class King extends Piece{
 
     @Override
     public ArrayList<Square> getLegalMoves(Square s ,Board b) {
-        return null;
+        ArrayList<Square> legalMoves = new ArrayList<>();
+        int x = s.getXNum();
+        int[] xs = new int[]{x - 1, x + 1, x};
+        int y = s.getYNum();
+        int[] ys = new int[]{y, y - 1, y + 1};
+        for (int xx : xs) {
+            for (int yy : ys) {
+                if (validXY(xx, yy))
+                    if (!isSameColorOnTheWay(b.getSquare(xx, yy)))
+                        legalMoves.add(b.getSquare(xx, yy));
+            }
+        }
+        return legalMoves;
+    }
+
+    private boolean validXY(int x, int y) {
+        return x >= 0 && x < 8 && y >= 0 && y < 8;
     }
 }
